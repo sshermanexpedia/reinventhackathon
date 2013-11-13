7c2b45544c2620fd995cec72e1ad4b49de558dce
girishp <girishp@expedia.com>
2013-11-12 18:00:32 -0800

    adding all the donor csv files

--- cookbooks/cookbooks/donordb/recipes/default.rb
+++ cookbooks/cookbooks/donordb/recipes/default.rb
@@ -27,6 +27,14 @@ script "install_donor_data" do
   code <<-EOH
   wget https://s3-us-west-2.amazonaws.com/reinventhackathon/04-open_data-projects.csv.gz
   gunzip 04-open_data-projects.csv.gz
+  wget https://s3-us-west-2.amazonaws.com/reinventhackathon/01-open_data-donations.csv.gz
+  gunzip 01-open_data-donations.csv.gz
+  wget https://s3-us-west-2.amazonaws.com/reinventhackathon/03-open_data-giftcards.csv.gz
+  gunzip 03-open_data-giftcards.csv.gz
+  wget https://s3-us-west-2.amazonaws.com/reinventhackathon/05-open_data-resources.csv.gz
+  gunzip 05-open_data-resources.csv.gz
+  wget https://s3-us-west-2.amazonaws.com/reinventhackathon/02-open_data-essays.csv.gz
+  gunzip 02-open_data-essays.csv.gz
   EOH
 end
 
