840aa3a4019042e22c313e8e067511b11cabc926
girishp <girishp@expedia.com>
2013-11-12 19:23:28 -0800

    modify the script to download donor data

--- cookbooks/cookbooks/donordb/recipes/default.rb
+++ cookbooks/cookbooks/donordb/recipes/default.rb
@@ -61,7 +61,7 @@ execute "create-root-user" do
   code = <<-EOH
 psql -U postgres -c "select * from pg_user where usename='root'" | grep -c root
   EOH
-  command "createuser -U postgres -s root"
+  command "sudo -u postgres createuser -s root"
   not_if code
 end
 
@@ -69,7 +69,7 @@ execute "create-database-user" do
   code = <<-EOH
 psql -U postgres -c "select * from pg_user where usename='#{node[:dbuser]}'" | grep -c #{node[:dbuser]}
   EOH
-  command "createuser -U postgres -sw #{node[:dbuser]}"
+  command "sudo  -u postgres createuser -sw #{node[:dbuser]}"
   not_if code
 end
 
@@ -78,7 +78,7 @@ execute "create-database" do
   exists = <<-EOH
 psql -U postgres -c "select * from pg_database WHERE datname='#{node[:dbname]}'" | grep -c #{node[:dbname]}
   EOH
-  command "createdb -U postgres -O #{node[:dbuser]} -E utf8 -T template0 #{node[:dbname]}"
+  command "sudo -u postgres createdb -O #{node[:dbuser]} -E utf8 -T template0 #{node[:dbname]}"
   not_if exists
 end
 
