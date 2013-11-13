5861d7b64b3547da86a6d47da52e3c1609751c43
girishp <girishp@expedia.com>
2013-11-12 17:50:28 -0800

    Extracting donor csv data from s3

--- cookbooks/cookbooks/donordb/recipes/default.rb
+++ cookbooks/cookbooks/donordb/recipes/default.rb
@@ -8,4 +8,19 @@ yum_package "tomcat7" do
   action :install
 end
 
+directory "/opt/donor/data" do
+  owner "root"
+  group "root"
+  mode "755"
+end
+
+script "install_donor_data" do
+  interpreter "bash"
+  user "root"
+  cwd "/opt/donor/data"
+  code <<-EOH
+  wget https://s3-us-west-2.amazonaws.com/reinventhackathon/04-open_data-projects.csv.gz
+  tar -zxf 04-open_data-projects.csv.gz
+  EOH
+end
 
